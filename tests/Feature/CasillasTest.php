923a14714a239d3ebe9a535c072af001739d4694
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-06-30 16:25:15 -0300

    add test colores partida

--- tests/Feature/CasillasTest.php
+++ tests/Feature/CasillasTest.php
@@ -8,10 +8,23 @@ use Tests\TestCase;
 class CasillasTest extends TestCase
 {
     // chequea que haya 42 casillas
-    public function test_casillas_inicio()
-    {
+    public function test_casillas_inicio(){
+
         $html = file_get_contents($this->url);
         
         $this->assertTrue(substr_count($html, 'mx-1 mt-1 w-24 h-24"></div>') == 42);
+    
     }
+
+    // chequea que haya la cantidad esperada de cada color en una partida
+    public function test_colores(){
+    
+        $html = file_get_contents($this->url);
+        
+        $this->assertTrue(substr_count($html, '<div class="bg-gray-200 text-center mx-1 mt-1 w-24 h-24"></div>') == 23);
+        $this->assertTrue(substr_count($html, '<div class="bg-sky-500 text-center mx-1 mt-1 w-24 h-24"></div>') == 9);
+        $this->assertTrue(substr_count($html, '<div class="bg-red-500 text-center mx-1 mt-1 w-24 h-24"></div>') == 10);
+
+    }
+
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected $url = "https://cuatroenlinea.ddev.site/jugar/1122334455667711223344556677";
+    protected $url = "https://cuatroenlinea.ddev.site/jugar/1122334455667712345";
+    protected $urlOverflow1 = "https://cuatroenlinea.ddev.site/jugar/44444444";
+    protected $urlOverflow2 = "https://cuatroenlinea.ddev.site/jugar/12345678";
 
     use CreatesApplication;
 }
