8114e063f3fb0966dbcc3dadc89309bf557a0f9f
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-08-22 22:17:48 -0300

    arreglo ssl url

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected $url = "https://cuatroenlinea.ddev.site/jugar/1122334455667712345";
-    protected $url_overflow_x = "https://cuatroenlinea.ddev.site/jugar/12345678";
-    protected $url_overflow_y = "https://cuatroenlinea.ddev.site/jugar/44444444";
+    protected $url = "http://cuatroenlinea.ddev.site/jugar/1122334455667712345";
+    protected $url_overflow_x = "http://cuatroenlinea.ddev.site/jugar/12345678";
+    protected $url_overflow_y = "http://cuatroenlinea.ddev.site/jugar/44444444";
 
     use CreatesApplication;
 }
