883920f42b19177764bc4384191c267f7b1711af
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2022-08-30 21:17:29 -0300

    ficha lista

--- app/Ficha.php
+++ app/Ficha.php
@@ -2,17 +2,24 @@
 
 namespace App;
 
-class Ficha {
+interface FichaInterface {
+	public function get_color() : string;
+}
+
+class Ficha implements FichaInterface {
 	protected $color;
 
     public function __construct( $color ){
-        $this->color = $color;
+        if( $color !== "🟥" && $color !== "🟦" ){
+			throw new \Exception("Solo se permiten fichas azules (🟦) o rojas (🟥)");
+		}
+		
+		$this->$color = $color;
     }
 
 	public function get_color(){
 		return $this->color;
 	}
-
 }
 
 ?>
